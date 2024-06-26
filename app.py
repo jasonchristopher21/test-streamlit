e58db788da7b0fcc3a9afb150e6cf31762ff66e6
Jason Christopher <96954436+jasonchristopher21@users.noreply.github.com>
2024-06-26 09:25:19 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+st.set_page_config(
+    page_title="Singtel Phishing Dashboard",
+    page_icon="👋",
+)
+
+st.write("""
+# My first app
+Hello *world!*
+""")
